4a33d5be29ef99be2ac487937aa29ddbc935bd53
letsgitcracking <45171676+letsgitcracking@users.noreply.github.com>
2019-07-15 21:05:00 +0100

    Added code done offline over-weekend

--- src/App.js
+++ src/App.js
@@ -1,36 +1,95 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div>
-      <h1>Trainers</h1>
-      <p>Please select the desired colour or size below:</p>
-      <img src="https://all.media.alexbooster.com/img/sneaker-orange.jpg" width="400" alt="Trainer"/>
-      <div>Size:
-        <select>
-          <option>7</option>
-          <option>8</option>
-          <option>9</option>
-          <option>10</option>
-          <option>11</option>
-          <option>12</option>
-        </select>
+class App extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      allColors: [],
+      allImagesObject: {},
+      bySizeObject: {},
+      listColorsObject: {},
+      sizesByColor: [],
+      selectedColor: "",
+      selectedSize: "9"
+    };
+  }
+
+  async componentDidMount() {
+    await fetch("https://all.data.alexbooster.com/json/sneakers.json")
+      .then(res => res.json())
+      .then(data => {
+        this.setState({
+          allColors: data[0].allColors.sort(),
+          allImagesObject: data[0].allImages,
+          bySizeObject: data[0].bySize,
+          listColorsObject: data[0].byColor,
+        })
+      }).catch(error => {
+        console.log(error);
+      });
+      await this.initialProduct();
+      this.setState({
+        sizesByColor: this.state.listColorsObject[this.state.selectedColor]
+      })
+  }
+
+  initialProduct = () => {
+    let orderedListSizes = this.state.bySizeObject[this.state.selectedSize].reduce((acc, val, idx) => {
+      let trainerObject = this.state.listColorsObject[this.state.bySizeObject[this.state.selectedSize][idx]].length;
+      acc[val] = trainerObject;
+      return acc;
+    }, {})
+
+    this.setState({
+      selectedColor: Object.keys(orderedListSizes).sort((a, b) => orderedListSizes[b] - orderedListSizes[a])[0]
+    })
+  }
+
+  handleOnChangeSize = (e) => {
+    this.setState({
+      selectedSize: e.target.value
+    });
+  }
+
+  handleOnChangeColor = async (e) => {
+    await this.setState({
+      selectedColor: e.target.value
+    });
+    this.updateSizes();
+  }
+
+  updateSizes = async () => {
+    await this.setState({
+      sizesByColor: this.state.listColorsObject[this.state.selectedColor]
+    });
+    this.setState({
+      selectedSize: this.state.sizesByColor[0]
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Trainers</h1>
+        <p>Select the desired colour or size below:</p>
+        <img src={this.state.allImagesObject[this.state.selectedColor]} width="400" alt={this.state.selectedColor.charAt(0).toUpperCase() + this.state.selectedColor.slice(1) + " Trainer"} />
+
+        <div>Size:
+          <select name="size" value={this.state.selectedSize} onChange={this.handleOnChangeSize}>
+            {Object.values(this.state.sizesByColor).map(size => <option key={size} value={size}>{size}</option>)}
+          </select>
+        </div>
+
+        <div>Colour:
+          <select name="color" value={this.state.selectedColor} onChange={this.handleOnChangeColor}>
+            {this.state.allColors.map(color => <option key={color} value={color}>{color.charAt(0).toUpperCase() + color.slice(1)}</option>)}
+          </select>
+        </div>
       </div>
-      <div>Colour:
-        <select>
-          <option>Aquamarine</option>
-          <option>Blue</option>
-          <option>Orange</option>
-          <option>Purple</option>
-          <option>Red</option>
-        </select>
-
-      </div>
-
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
